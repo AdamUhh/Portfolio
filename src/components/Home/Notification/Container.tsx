bd849a31dc4dd189cf718c8c05be8b6383018056
AdamUhh <adamuhh.dev@gmail.com>
2023-11-25 01:10:54 +0400

    fixed styling in home notification

--- src/components/Home/Notification/Container.tsx
+++ src/components/Home/Notification/Container.tsx
@@ -15,9 +15,13 @@ export default function NotificationContainer({
     <Draggable handle=".draggable_handle" nodeRef={nodeRef}>
       <div
         className={cn(
-          "left-[35%] top-[15%] h-fit min-h-[250px] sm:min-h-[350px] w-[40%] min-w-[300px] max-w-[500px]  rounded-2xl bg-gradient-to-r from-[#e0edf8] to-[#d6deef]  shadow-lg shadow-black lg:w-[60%] sm:left-[5%]",
+          "left-[35%] top-[15%] h-fit min-h-[200px] w-[40%] min-w-[300px] max-w-[500px] rounded-2xl  bg-gradient-to-r from-[#e0edf8] to-[#d6deef] shadow-lg  shadow-black lg:w-[60%] sm:left-[5%] sm:min-h-[200px]",
           isOpen ? "fixed" : "hidden",
         )}
+        // className={cn(
+        //   "left-[35%] top-[15%] h-fit min-h-[250px] sm:min-h-[350px] w-[40%] min-w-[300px] max-w-[500px]  rounded-2xl bg-gradient-to-r from-[#e0edf8] to-[#d6deef]  shadow-lg shadow-black lg:w-[60%] sm:left-[5%]",
+        //   isOpen ? "fixed" : "hidden",
+        // )}
         ref={nodeRef}
       >
         {children}
