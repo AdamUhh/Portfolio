014fd45e991664104e0039fdcabd471839238993
AdamUhh <adamuhh.dev@gmail.com>
2024-06-22 12:13:18 +0400

    added no cached function on calculate age

--- src/utils/calculateAge.ts
+++ src/utils/calculateAge.ts
@@ -1,22 +1,24 @@
 // Note: Months are zero-indexed (0 is January, 5 is June)
+import { unstable_noStore as noStore } from 'next/cache';
 function calculateAge(birthdate: Date = new Date(2002, 5, 5)): number {
-  const currentDate = new Date();
+    noStore()
+    const currentDate = new Date();
 
-  const birthYear = birthdate.getFullYear();
-  const currentYear = currentDate.getFullYear();
+    const birthYear = birthdate.getFullYear();
+    const currentYear = currentDate.getFullYear();
 
-  let age = currentYear - birthYear;
+    let age = currentYear - birthYear;
 
-  const birthMonth = birthdate.getMonth();
-  const currentMonth = currentDate.getMonth();
+    const birthMonth = birthdate.getMonth();
+    const currentMonth = currentDate.getMonth();
 
-  if (
-    currentMonth < birthMonth ||
-    (currentMonth === birthMonth && currentDate.getDate() < birthdate.getDate())
-  ) {
-    age--;
-  }
+    if (
+        currentMonth < birthMonth ||
+        (currentMonth === birthMonth && currentDate.getDate() < birthdate.getDate())
+    ) {
+        age--;
+    }
 
-  return age;
+    return age;
 }
 export { calculateAge };
